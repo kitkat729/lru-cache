be5fb30e3f6387fedeba8ee361369eac9b37b5d8
Hoi Kit Yuen <hoikityuen@gmail.com>
2022-10-04 02:49:05 -0400

    feat: Core implementation (#2)
    * build: output js to lib folder
    
    * build: add code coverage for tests
    
    * feat: core implementation
    
    * test: add unit tests
    
    * feat: Update default values; change class property scope
    
    * feat: use min capacity
    
    * docs: update Readme
    
    * test: update test cases
    
    * docs: styling
    
    * docs: styling
    
    * docs: styling
    
    * build: remove comments from js outputs
    
    * chore: clean logic
    
    * chore: consolidate code
    
    * chore: clean code
    
    * chore: clean code; indentations
    
    * chore: temporarily remove esbuild
    
    * chore: remove unused entries
    
    * build: separate lint fix as a different command
    
    * build: git action release workflow
    
    * build: add a scope for package

--- README.md
+++ README.md
@@ -1,8 +1,47 @@
-# Npm package boilerplate for typescript
+# LRU cache
 
-Boilerplate includes support for
-- [Typescript transpiled by esbuild](https://esbuild.github.io/)
-- [Eslint](https://eslint.org/blog/2022/01/eslint-v8.8.0-released/#main)
-- [Prettier](https://prettier.io/docs/en/index.html)
-- [Jest](https://jestjs.io/docs/getting-started)
-- [Code coverage](https://www.npmjs.com/package/codecov)
+## What is LRU cache?
+LRU cache is a data structure that organizes data by access order. The most recently created and most accessed data stay in the front of the cache. The least accessed data gets pushed behind and eventually out of the cache as new data is added.
+
+LRU cache is optimized for quick data access. It contains a map of keys pointing to its data. Given a valid key, the cache returns the data in O(1) time. It is also optimized for space. Imagine the cache lives in a space where it can only hold so much data. The cache wants to keep the most accessed data and tosses away the least accessed data. LRU gives you that. LRU accepts a capacity parameter to limit the amount of data it can keep.
+
+## How to use?
+Example:
+const cache = new LruCache<string, string>(100);
+
+Explanation:
+The code creates a cache instance with a capacity of 100. 100 is the max number of keys the cache can hold. The key will be of type string and the value will be of type string. To take new data, it must get rid of some old ones. At the end, the max size will remain 100, given the capacity remains constant.
+
+Other examples
+- LruCache<string, int>(N)
+- LruCache<int, int>(N)
+- LruCache<int, boolean>(N)
+- LruCache<string, CustomType>(N)
+
+* Min capacity is 1 if smaller than 1 capacity is given.
+* Default capacity will be used if no capacity is specified
+
+## Methods
+put(key, value)
+- Add an item to the cache
+
+get(key)
+- Get the item associated with the key from the cache. Return undefined if key is invalid or no data is found
+
+init()
+- Reinitialize the cache
+
+clear()
+- Remove all items from the cache.
+
+isFull()
+- Returns true if size equals to capacity
+
+getMruValue()
+- Get the most recently used value from cache
+
+getLruValue()
+- Get the least recently used value from cache
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,199 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+const MIN_CAPACITY = 1;
+const DEFAULT_CAPACITY = MIN_CAPACITY;
+
+interface ListNode<K, V> {
+  key: K;
+  value: V | undefined;
+  prev: ListNode<K, V> | null;
+  next: ListNode<K, V> | null;
+}
+
+class LruCache<K, V> {
+  private _capacity = MIN_CAPACITY;
+  private storageHead: ListNode<K, V> | null;
+  private storageTail: ListNode<K, V> | null;
+  private map: Map<K, ListNode<K, V>>;
+  private _size = 0;
+
+  constructor(capacity = DEFAULT_CAPACITY) {
+    this.capacity = capacity;
+    this.storageHead = null; // most accessed node stored at the front
+    this.storageTail = null;
+    this.map = new Map<K, ListNode<K, V>>();
+  }
+
+  protected set capacity(capacity: number) {
+    this._capacity = capacity < MIN_CAPACITY ? MIN_CAPACITY : capacity;
+  }
+
+  protected get capacity() {
+    return this._capacity;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  init() {
+    this.storageHead = null; // most accessed node stored at the front
+    this.storageTail = null;
+    this.map.clear();
+    this._size = 0;
+  }
+
+  /**
+   * Insert new key with item or overwrite existing key with item
+   *
+   * @param {K} key
+   * @param {V | undefined} value
+   * @return {boolean} True if successful, otherwise false
+   */
+  put(key: K, value: V | undefined) {
+    let node = this.map.has(key) ? this.map.get(key) : null;
+    let isSuccess = false;
+
+    // Considerations:
+    // key exists
+    // _capacity is full
+    // _capacity is 0
+
+    if (node) {
+      node.value = value;
+      this.storageHead && this.moveNodeBefore(node, this.storageHead);
+      isSuccess = true;
+    } else {
+      // Cache not empty and reached capacity
+      if (this.isFull() && this.storageTail) {
+        // bring the size below the capacity by removing a least recently used item
+        const n = this.removeNode(this.storageTail);
+
+        n.key && this.map.delete(n.key);
+        this._size--;
+      }
+
+      if (this._size < this.capacity) {
+        node = { key, value } as ListNode<K, V>;
+        this.prependNode(node);
+        this._size++;
+        this.map.set(key, node);
+        isSuccess = true;
+      }
+    }
+
+    return isSuccess;
+  }
+
+  /**
+   * Get the item associated with the key
+   *
+   * @param {K} key
+   * @return {V | undefined}
+   */
+  get(key: K): V | undefined {
+    const node = this.map.has(key) ? this.map.get(key) : null;
+
+    if (node) {
+      this.storageHead && this.moveNodeBefore(node, this.storageHead);
+
+      return node.value;
+    }
+
+    return undefined;
+  }
+
+  isFull() {
+    return this._size === this.capacity;
+  }
+
+  /**
+   * Add the node to the list front
+   *
+   * @param {ListNode} node
+   * @return {ListNode}
+   */
+  protected prependNode(node: ListNode<K, V>) {
+    if (this.storageHead) {
+      this.storageHead.prev = node;
+      node.next = this.storageHead;
+    }
+    this.storageHead = node;
+    if (!this.storageTail) {
+      this.storageTail = node;
+    }
+
+    return node;
+  }
+
+  /**
+   * Remove the node from list
+   *
+   * @param {ListNode} node
+   * @return {ListNode}
+   */
+  protected removeNode(node: ListNode<K, V>) {
+    // Detach the node from the list
+    if (node.prev) node.prev.next = node.next;
+    if (node.next) node.next.prev = node.prev;
+
+    // Adjust head and tail pointers
+    if (this.storageHead === node) {
+      this.storageHead = node.next;
+    }
+    if (this.storageTail === node) {
+      this.storageTail = node.prev;
+    }
+
+    node.prev = null;
+    node.next = null;
+
+    return node;
+  }
+
+  /**
+   * Move the node in front of the target node. Does not increment size.
+   *
+   * @param {ListNode} node
+   * @param {ListNode} target
+   */
+  protected moveNodeBefore(node: ListNode<K, V>, target: ListNode<K, V>) {
+    if (node === target) return;
+
+    // Node in the correct position
+    if (node.next === target) return;
+
+    node = this.removeNode(node);
+
+    // Attach node
+    // links between node and the node before target
+    if (target.prev) {
+      // This condition is not reachable if the target node is always the head node.
+      target.prev.next = node;
+    }
+    node.prev = target.prev;
+
+    // links between node and target
+    target.prev = node;
+    node.next = target;
+
+    if (this.storageHead === target) {
+      this.storageHead = node;
+    }
+  }
+
+  getMruValue() {
+    return this.storageHead ? this.storageHead.value : undefined;
+  }
+
+  getLruValue() {
+    return this.storageTail ? this.storageTail.value : undefined;
+  }
+
+  /**
+   * Remove all cached items
+   */
+  clear() {
+    this.init();
+  }
+}
+
+export default LruCache;

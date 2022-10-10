6abe5d577a0e1892d69475fd104e06d3891ec518
Hoi Yuen <hoikityuen@gmail.com>
2022-10-10 15:51:50 -0400

    build(config): build js outputs and type declarations

--- build.js
+++ build.js
@@ -1,7 +1,8 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
 const { build } = require('esbuild');
-const { Generator } = require('npm-dts');
+const { Generator, ELogLevel } = require('npm-dts');
 const path = require('path');
+const { stat } = require('fs');
 const { dependencies, peerDependencies } = require('./package.json');
 
 const config = {
@@ -9,6 +10,7 @@ const config = {
   platform: 'node', // cjs
   bundle: true,
   minify: true,
+  target: 'es6',
   outdir: './lib',
   external: Object.keys(dependencies ?? {}).concat(Object.keys(peerDependencies ?? {})),
   logLevel: 'info',
@@ -20,12 +22,28 @@ const buildTypeFile = (entry) => {
   }
 
   const file = path.parse(entry);
+  const outfile = `${file.name}.d.ts`;
 
-  new Generator({
-    entry: file.base,
-    output: path.join(config.outdir, `${file.name}.d.ts`),
-  }).generate();
+  new Generator(
+    {
+      entry: file.base,
+      output: path.join(config.outdir, outfile),
+      logLevel: ELogLevel.info,
+    },
+    false,
+    true,
+  )
+    .generate()
+    .then(() => {
+      stat(path.join(config.outdir, outfile), (err, stats) => {
+        if (err) {
+          throw err;
+        }
+        console.log(`  ${path.join(config.outdir, outfile)}  ${stats.size}b`);
+      });
+    });
 };
 
-build(config);
 config.entryPoints.forEach(buildTypeFile);
+
+build(config);
